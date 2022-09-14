f16a2e4f4ee77d7b5a9633e5fee45625a8e5ce7f
bodioben miguel <miguelbodio@gmail.com>
2022-09-14 03:37:14 +0200

    add brand section

--- src/Components/Brand.jsx
+++ src/Components/Brand.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import styles from '../styles.js'
+import { dropbox, coinbase, airbnb, google, binance, apple } from '../assets'
+
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className={`brand__section ${styles.paddingF} flex flex-wrap justify-center items-center`}>
+      <div className={`${styles.flexCenter} min-w-[120px] max-w-[150px] m-4 flex-1 `}>
+        <img src={airbnb} alt="google" />
+      </div>
+      <div className={`${styles.flexCenter} min-w-[120px] max-w-[150px] m-4 flex-1 `}>
+        <img src={binance} alt="google" />
+      </div>
+      <div className={`${styles.flexCenter} min-w-[120px] max-w-[150px] m-4 flex-1 `}>
+        <img src={dropbox} alt="google" />
+      </div>
+      <div className={`${styles.flexCenter} min-w-[120px] max-w-[150px] m-4 flex-1 `}>
+        <img src={coinbase} alt="google" />
+      </div>
+    </div>
   )
 }
 
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,18 @@
 import robot from '../assets/robot.png'
+import coinbase from '../assets/coinbase.png'
+import airbnb from '../assets/airbnb.png'
+import binance from '../assets/binance.png'
+import dropbox from '../assets/dropbox.png'
+import google from '../assets/google.svg'
+import apple from '../assets/apple.svg'
+
 
 export{
-    robot
+    robot,
+    apple,
+    airbnb,
+    coinbase,
+    dropbox,
+    binance,
+    google
 }
